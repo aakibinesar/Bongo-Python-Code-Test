efbf01f5f593a851d3a9fc6141b07ce239343950
Aakib Bin Nesar <aakibinesar@users.noreply.github.com>
2020-07-29 21:37:15 +0100

    Delete Test2.py

--- Test2.py
+++
@@ -1,9 +0,0 @@
-import unittest
-import Problem_2
-
-class TaskTest(unittest.TestCase):
-	def test1(self):
-		self.assertEqual(Problem_2.print_depth(Problem_2.D),('key1',1))
-
-if __name__ == '__main__':
-	unittest.main()
